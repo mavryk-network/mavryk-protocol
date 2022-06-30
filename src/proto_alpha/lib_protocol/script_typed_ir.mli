23d6906d5063b424b2b8ad7f3987e9bf623ab3f8
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2022-06-30 07:42:57 +0200

    Proto/Michelson: add log_kinstr to logger

--- src/proto_alpha/lib_protocol/script_typed_ir.mli
+++ src/proto_alpha/lib_protocol/script_typed_ir.mli
@@ -1324,6 +1324,7 @@ and logger = {
           produced. *)
   klog : 'a 's 'r 'f. ('a, 's, 'r, 'f) klog;
   ilog : 'a 's 'b 't 'r 'f. ('a, 's, 'b, 't, 'r, 'f) ilog;
+  log_kinstr : 'a 'b 'c 'd. ('a, 'b, 'c, 'd) log_kinstr;
 }
 
 and ('a, 's, 'r, 'f) klog =
@@ -1360,6 +1361,12 @@ and ('a, 's, 'b, 't, 'r, 'f) step_type =
   * Local_gas_counter.local_gas_counter)
   tzresult
   Lwt.t
+
+and ('a, 'b, 'c, 'd) log_kinstr =
+  logger ->
+  ('a, 'b) stack_ty ->
+  ('a, 'b, 'c, 'd) kinstr ->
+  ('a, 'b, 'c, 'd) kinstr
 
 (* ---- Auxiliary types -----------------------------------------------------*)
 and ('ty, 'comparable) ty =
