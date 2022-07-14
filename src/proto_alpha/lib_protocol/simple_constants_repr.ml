2aa18fc970ec1a32d12af7f5214fe5bffec87349
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2022-07-14 09:29:12 +0200

    make wrapped

--- src/proto_alpha/lib_protocol/simple_constants_repr.ml
+++ src/proto_alpha/lib_protocol/simple_constants_repr.ml
@@ -1,17 +1,43 @@
-type t = {
-  preserved_cycles : int;
-  hard_gas_limit_per_operation : Gas_limit_repr.Arith.integral;
-}
+module MAKE =
+functor
+  (W : sig
+     type 'a w
 
-let encoding : t Data_encoding.t =
-  let open Data_encoding in
-  conv
-    (fun {preserved_cycles; hard_gas_limit_per_operation} ->
-      (preserved_cycles, hard_gas_limit_per_operation))
-    (fun (preserved_cycles, hard_gas_limit_per_operation) ->
-      {preserved_cycles; hard_gas_limit_per_operation})
-    (obj2
-       (req "preserved_cycles" uint8)
-       (req
-          "hard_gas_limit_per_operation"
-          Gas_limit_repr.Arith.z_integral_encoding))
+     val w : 'a Data_encoding.t -> 'a w Data_encoding.t
+   end)
+  ->
+  struct
+    open W
+
+    type t = {
+      preserved_cycles : int w;
+      hard_gas_limit_per_operation : Gas_limit_repr.Arith.integral w;
+    }
+
+    let encoding : t Data_encoding.t =
+      let open Data_encoding in
+      conv
+        (fun {preserved_cycles; hard_gas_limit_per_operation} ->
+          (preserved_cycles, hard_gas_limit_per_operation))
+        (fun (preserved_cycles, hard_gas_limit_per_operation) ->
+          {preserved_cycles; hard_gas_limit_per_operation})
+        (obj2
+           (req "preserved_cycles" (w uint8))
+           (req
+              "hard_gas_limit_per_operation"
+              (w Gas_limit_repr.Arith.z_integral_encoding)))
+  end
+
+module Unwrapped = MAKE (struct
+  type 'a w = 'a
+
+  let w e = e
+end)
+
+module Wrapped = MAKE (struct
+  type 'a w = 'a option
+
+  let w e = Data_encoding.option e
+end)
+
+include Unwrapped
