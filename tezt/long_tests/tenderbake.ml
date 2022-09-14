3e6e00c38503d601a49cead624017fc6f67cadf0
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2022-09-14 15:35:26 +0200

    add bakers restart

--- tezt/long_tests/tenderbake.ml
+++ tezt/long_tests/tenderbake.ml
@@ -47,85 +47,7 @@ let time_to_reach_measurement = sf "time-to-reach-%d" levels
 let test_rounds_title =
   sf "check that we reach level %d on all %d nodes" levels nodes_num
 
-type topology = Clique | Ring
-
-let cluster_of_topology = function
-  | Clique -> Cluster.clique
-  | Ring -> Cluster.ring
-
-let string_of_topology = function Clique -> "clique" | Ring -> "ring"
-
-let test_long_dynamic_bake_title topology =
-  "long dynamic bake: " ^ string_of_topology topology
-
-let dynamic_bake_test_cycles =
-  Sys.getenv_opt "TEZT_LONG_TEST_LONG_DYNAMIC_TEST_CYCLES"
-  |> Option.map int_of_string |> Option.value ~default:60
-
-let dynamic_bake_test_blocks_per_cycles =
-  Sys.getenv_opt "TEZT_LONG_TEST_LONG_DYNAMIC_BLOCKS_PER_CYCLES"
-  |> Option.map int_of_string |> Option.value ~default:2
-
-(* add one for genesis block *)
-let dynamic_bake_max_level =
-  1 + (dynamic_bake_test_cycles * dynamic_bake_test_blocks_per_cycles)
-
-let dynamic_bake_time_to_reach_max_level_measurement =
-  sf "dynamic-bake-time-to-reach-%d" dynamic_bake_max_level
-
-(* that is, decision expected in at most 3 rounds. only used for display *)
-let dynamic_bake_expected_max_rounds = 3
-
-let dynamic_bake_minimal_block_delay = 1
-
-let dynamic_bake_delay_increment_per_round = 1
-
-(* c.f. https://tezos.gitlab.io/active/consensus.html *)
-let dynamic_bake_round_duration r =
-  dynamic_bake_minimal_block_delay + (r * dynamic_bake_delay_increment_per_round)
-
-let dynamic_bake_grafana_panels topology =
-  let test = test_long_dynamic_bake_title topology in
-  let open Grafana in
-  [
-    Row ("Test: " ^ test_long_dynamic_bake_title topology);
-    simple_graph
-      ~title:
-        (sf
-           "The time it takes the cluster to reach level %d"
-           dynamic_bake_max_level)
-      ~yaxis_format:" s"
-      ~measurement:dynamic_bake_time_to_reach_max_level_measurement
-      ~field:"duration"
-      ~test
-      ();
-    graphs_per_tags
-      ~title:"The time it takes node 0 to reach level N."
-      ~yaxis_format:" s"
-      ~measurement:"node-0-reaches-level"
-      ~field:"duration"
-      ~test
-      ~tags:
-        (List.map (fun level -> ("level", string_of_int level))
-        @@ range 2 dynamic_bake_max_level)
-      ();
-    graphs_per_tags
-      ~title:
-        (sf
-           "Round in which consensus was reached on level N (should be less \
-            than %d all the way through)."
-           dynamic_bake_expected_max_rounds)
-      ~yaxis_format:" rounds"
-      ~measurement:"node-0-reaches-level"
-      ~field:"round"
-      ~test
-      ~tags:
-        (List.map (fun level -> ("level", string_of_int level))
-        @@ range 2 dynamic_bake_max_level)
-      ();
-  ]
-
-let grafana_panels : Grafana.panel list =
+let rounds_grafana_panels : Grafana.panel list =
   let open Grafana in
   [
     Row ("Test: " ^ test_rounds_title);
@@ -158,8 +80,6 @@ let grafana_panels : Grafana.panel list =
         @@ range 2 levels)
       ();
   ]
-  @ dynamic_bake_grafana_panels Clique
-  @ dynamic_bake_grafana_panels Ring
 
 (* Check that in a simple ring topology with as many nodes/bakers/clients as
    bootstrap accounts, each with a single delegate, all blocks occurs within
@@ -360,6 +280,86 @@ module Inf = struct
 
   let next : 'a t -> 'a * 'a t = function Inf f -> f ()
 end
+
+type topology = Clique | Ring
+
+let cluster_of_topology = function
+  | Clique -> Cluster.clique
+  | Ring -> Cluster.ring
+
+let string_of_topology = function Clique -> "clique" | Ring -> "ring"
+
+let test_long_dynamic_bake_title topology =
+  "long dynamic bake: " ^ string_of_topology topology
+
+let dynamic_bake_test_cycles =
+  Sys.getenv_opt "TEZT_LONG_TEST_LONG_DYNAMIC_TEST_CYCLES"
+  |> Option.map int_of_string |> Option.value ~default:60
+
+let dynamic_bake_test_blocks_per_cycles =
+  Sys.getenv_opt "TEZT_LONG_TEST_LONG_DYNAMIC_BLOCKS_PER_CYCLES"
+  |> Option.map int_of_string |> Option.value ~default:2
+
+(* add one for genesis block *)
+let dynamic_bake_max_level =
+  1 + (dynamic_bake_test_cycles * dynamic_bake_test_blocks_per_cycles)
+
+let dynamic_bake_time_to_reach_max_level_measurement =
+  sf "dynamic-bake-time-to-reach-%d" dynamic_bake_max_level
+
+(* that is, decision expected in at most 3 rounds. only used for display *)
+let dynamic_bake_expected_max_rounds = 3
+
+let dynamic_bake_minimal_block_delay = 1
+
+let dynamic_bake_delay_increment_per_round = 1
+
+(* c.f. https://tezos.gitlab.io/active/consensus.html *)
+let dynamic_bake_round_duration r =
+  dynamic_bake_minimal_block_delay + (r * dynamic_bake_delay_increment_per_round)
+
+let test_bakers_restart_title = "bakers restart"
+
+let dynamic_bake_grafana_panels topology =
+  let test = test_long_dynamic_bake_title topology in
+  let open Grafana in
+  [
+    Row ("Test: " ^ test_long_dynamic_bake_title topology);
+    simple_graph
+      ~title:
+        (sf
+           "The time it takes the cluster to reach level %d"
+           dynamic_bake_max_level)
+      ~yaxis_format:" s"
+      ~measurement:dynamic_bake_time_to_reach_max_level_measurement
+      ~field:"duration"
+      ~test
+      ();
+    graphs_per_tags
+      ~title:"The time it takes node 0 to reach level N."
+      ~yaxis_format:" s"
+      ~measurement:"node-0-reaches-level"
+      ~field:"duration"
+      ~test
+      ~tags:
+        (List.map (fun level -> ("level", string_of_int level))
+        @@ range 2 dynamic_bake_max_level)
+      ();
+    graphs_per_tags
+      ~title:
+        (sf
+           "Round in which consensus was reached on level N (should be less \
+            than %d all the way through)."
+           dynamic_bake_expected_max_rounds)
+      ~yaxis_format:" rounds"
+      ~measurement:"node-0-reaches-level"
+      ~field:"round"
+      ~test
+      ~tags:
+        (List.map (fun level -> ("level", string_of_int level))
+        @@ range 2 dynamic_bake_max_level)
+      ();
+  ]
 
 (* This test runs [num_nodes_with_bakers + num_nodes_without_bakers]
    bakers, and [num_nodes_with_bakers] bakers. It runs
@@ -608,7 +608,193 @@ let test_long_dynamic_bake topology ~executors =
 
   Lwt.return time
 
+(* Go around twice to kill each baker once *)
+let bakers_restart_num_test_cycles = 2 * Array.length Account.Bootstrap.keys
+
+let bakers_restart_cycle_dur = 10
+
+let bakers_restart_minimal_block_delay = 4
+
+let bakers_restart_timeout =
+  bakers_restart_cycle_dur * bakers_restart_num_test_cycles
+
+let bakers_restart_expected_min_level = 2 + (bakers_restart_num_test_cycles / 2)
+
+let bakers_restart_grafana_panels : Grafana.panel list =
+  let open Grafana in
+  [
+    Row ("Test: " ^ test_rounds_title);
+    simple_graph
+      ~title:
+        (sf
+           "Level reached in %d cycles (expected: %d)"
+           bakers_restart_num_test_cycles
+           bakers_restart_expected_min_level)
+      ~yaxis_format:" levels"
+      ~measurement:time_to_reach_measurement
+      ~test:test_rounds_title
+      ~field:"duration"
+      ();
+  ]
+
+let test_bakers_restart ~executors =
+  let num_nodes = Array.length Account.Bootstrap.keys in
+  (* [bakers_restart_cycle_dur] should be correlated with [minimal_block_delay] and *)
+  (* [delay_increment_per_round] below. it should be long enough for
+     the bakers to take a decision in each round. *)
+  let topology = Clique in
+
+  (* A vague over approximation *)
+  let timeout =
+    (bakers_restart_minimal_block_delay + bakers_restart_cycle_dur)
+    * bakers_restart_num_test_cycles
+  in
+
+  Long_test.register
+    ~__FILE__
+    ~title:test_bakers_restart_title
+    ~tags:["tenderbake"; "dynamic"; "restart"]
+    ~executors
+    ~timeout:(Long_test.Seconds (8 * timeout))
+  @@ fun () ->
+  Log.info "Generating test-specific parameter file" ;
+  let* parameter_file =
+    let stringify_int n = Some (sf "\"%d\"" n) in
+    let base = Either.Right (protocol, None) in
+    let original_parameters_file = Protocol.parameter_file protocol in
+    let parameters = JSON.parse_file original_parameters_file in
+    (* TODO: see weirdness discussed above
+    *)
+    let consensus_threshold =
+      let _consensus_committee_size =
+        JSON.(get "consensus_committee_size" parameters |> as_int)
+      in
+      (* (consensus_committee_size * 2 / 3) + 1 *)
+      31
+    in
+
+    Protocol.write_parameter_file
+      ~base
+      [
+        (["minimal_block_delay"], stringify_int dynamic_bake_minimal_block_delay);
+        ( ["delay_increment_per_round"],
+          stringify_int dynamic_bake_delay_increment_per_round );
+        (["consensus_threshold"], Some (string_of_int consensus_threshold));
+      ]
+  in
+
+  Long_test.measure_and_check_regression_lwt
+    ~repeat
+    dynamic_bake_time_to_reach_max_level_measurement
+  @@ fun () ->
+  Log.info "Setup daemons" ;
+  let node_daemons = ref [] in
+  let baker_daemons = ref [] in
+
+  (* One client to activate the protocol later on *)
+  let* node_hd, activator_client, nodes_tl, _clients_tl =
+    let create ?(add_baker = true) i =
+      let node = Node.create [Synchronisation_threshold 0] in
+      Log.info
+        "Creating node, client%s triplet #%d (%s)"
+        (if add_baker then ", baker" else "")
+        i
+        (Node.name node) ;
+      node_daemons := node :: !node_daemons ;
+      let* client = Client.init ~endpoint:(Client.Node node) () in
+      let delegates = [Account.Bootstrap.keys.(i mod len).alias] in
+      let* () =
+        if add_baker then (
+          let baker = Baker.create ~protocol node client ~delegates in
+          let* () = Baker.run baker in
+          baker_daemons := baker :: !baker_daemons ;
+          unit)
+        else unit
+      in
+      Lwt.return (node, client)
+    in
+    let* node_with_baker_hd, activator_client = create 0 in
+    let* nodes_with_bakers = Lwt_list.map_s create (range 1 (num_nodes - 1)) in
+    return
+      ( node_with_baker_hd,
+        activator_client,
+        List.map fst nodes_with_bakers,
+        List.map snd nodes_with_bakers )
+  in
+  let nodes = node_hd :: nodes_tl in
+
+  Log.info
+    "Setting up node %s topology: %s"
+    (string_of_topology topology)
+    (String.concat ", " @@ List.map Node.name nodes) ;
+  (cluster_of_topology topology) nodes ;
+
+  Log.info "Starting nodes" ;
+  let* () = Cluster.start ~wait_connections:true nodes in
+
+  Log.info "Activating protocol" ;
+  let* () =
+    Client.activate_protocol_and_wait
+      ~protocol
+      ~timestamp:Client.Now
+      ~parameter_file
+      activator_client
+  in
+
+  let bakers = Inf.cycle !baker_daemons in
+
+  let rec loop cycle (dead_baker, bakers) =
+    if cycle < bakers_restart_num_test_cycles then
+      if
+        (* in the even cycles we run all the bakers. in the odd cycles
+           one is dead. we iterate cyclically through the bakers to
+           choose the dead one *)
+        cycle mod 2 = 0
+      then (
+        Log.info "Cycle %d: kill %s" cycle (Baker.name dead_baker) ;
+        let* () = Baker.terminate dead_baker in
+        let* () =
+          Lwt_unix.sleep (float_of_int bakers_restart_minimal_block_delay)
+        in
+        loop (cycle + 1) (dead_baker, bakers))
+      else
+        let* () =
+          if cycle > 1 then (
+            Log.info "Cycle %d: revive %s" cycle (Baker.name dead_baker) ;
+            let* () = Baker.run dead_baker in
+            unit)
+          else (
+            Log.info "Cycle %d: do nothing" cycle ;
+            unit)
+        in
+        let* () = Lwt_unix.sleep (float_of_int bakers_restart_cycle_dur) in
+        loop (cycle + 1) (Inf.next bakers)
+    else unit
+  in
+
+  let* () = loop 0 (Inf.next bakers) in
+  let levels = List.map Node.get_level nodes in
+  let min_level = List.fold_left Int.min Int.max_int levels in
+  Log.info
+    "Minimium level in cluster <%d>, expected: %d. Levels per node: [%s]"
+    min_level
+    bakers_restart_expected_min_level
+    (String.concat ", " (List.map string_of_int levels)) ;
+
+  (* Clean up for repeat *)
+  let* () = Lwt_list.iter_p Baker.terminate !baker_daemons in
+  let* () = Lwt_list.iter_p Node.terminate !node_daemons in
+
+  Lwt.return (float_of_int min_level)
+
+let grafana_panels : Grafana.panel list =
+  rounds_grafana_panels
+  @ dynamic_bake_grafana_panels Clique
+  @ dynamic_bake_grafana_panels Ring
+  @ bakers_restart_grafana_panels
+
 let register ~executors () =
   test_rounds ~executors ;
   test_long_dynamic_bake Clique ~executors ;
-  test_long_dynamic_bake Ring ~executors
+  test_long_dynamic_bake Ring ~executors ;
+  test_bakers_restart ~executors
