55eeda64e675c953efe82b88faee268e10297b7b
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2022-10-07 13:07:59 +0200

    lazy and arbitrary parameters

--- tezt/lib_core/parametric.ml
+++ tezt/lib_core/parametric.ml
@@ -59,3 +59,88 @@ let strings ?name values = list ?name ~to_string:Fun.id values
 let ints ?name values = list ?name ~to_string:string_of_int values
 
 let parameterize (param : 'a param) f = f param
+
+module Lazy = struct
+  let list ?name values =
+    let to_string (value_tag, _thunk) = value_tag in
+    {
+      name;
+      to_string;
+      values;
+      tags = (fun v -> [to_string v]);
+      title_tag = (fun v -> Some (to_string v));
+      title_prefix = Fun.const None;
+    }
+end
+
+(* This does not compose well with regression tests, as there is no
+   mapping between the test's title and the values that are generated.
+
+   If we put values instead of indexes in the titles, we get this
+   mapping, but we have to make sure that titles are unique (no
+   repeated generated values). Furthermore, it might be tricky to
+   ensure that the state of the random generator is "stable". This
+   means that the set of tests (the set of title tests), will
+   fluctuate, which I do not think is a good idea.
+*)
+
+module Arbitrary = struct
+  let n = 3
+
+  let arb ?name ~to_string gen =
+    let st = Random.State.make [||] in
+    let values = List.init n (fun idx -> (idx, gen st)) in
+    let to_string (_idx, value) = to_string value in
+    {
+      name;
+      to_string;
+      values;
+      (* I don't think tags make sense here *)
+      tags = (fun _ -> ["arbitrary"]);
+      title_tag = (fun (idx, _v) -> Some (sf "#%d" idx));
+      title_prefix = Fun.const None;
+    }
+
+  let int = arb ~to_string:string_of_int (fun st -> Random.State.int st 10)
+
+  let float =
+    arb ~to_string:string_of_float (fun st -> Random.State.float st 10.0)
+
+  (* With this setup, a pair becomes the cross-product of the values
+     of the underlying params. This might be unexpected, it is not how
+     e.g. QCheck does it.  There, the componetns of the pair is
+     individually generated. To resolve this, I think
+     we'd have to stratify [arb] into [gen] and [arb] like QCheck does.
+
+     However, at that point, why not use the QCheck generators?
+  *)
+  let pair (p1 : (int * 'a) param) (p2 : (int * 'b) param) =
+    let values =
+      List.concat_map
+        (fun (_idx1, v1) -> List.map (fun (_idx2, v2) -> (v1, v2)) p2.values)
+        p1.values
+    in
+    let values = List.mapi (fun i v -> (i, v)) values in
+    {
+      name = None;
+      to_string =
+        (fun (_idx, (v1, v2)) ->
+          (* the zeroes here are unfortunate but required to get the underlying to_string *)
+          sf "%s,%s" (p1.to_string (0, v1)) (p2.to_string (0, v2)));
+      values;
+      (* I don't think tags make sense here *)
+      tags = (fun _ -> ["arbitrary"]);
+      title_tag = (fun (idx, _v) -> Some (sf "#%d" idx));
+      title_prefix = Fun.const None;
+    }
+
+  let of_qcheck ?name (qcheck_arb : 'a QCheck.arbitrary) =
+    let gen = QCheck.get_gen qcheck_arb in
+    let to_string =
+      match QCheck.get_print qcheck_arb with
+      (* A wart *)
+      | None -> fun _ -> ""
+      | Some to_string -> to_string
+    in
+    arb ?name ~to_string gen
+end
--- tezt/self_tests/test_parametric.ml
+++ tezt/self_tests/test_parametric.ml
@@ -71,4 +71,68 @@ let register () =
            unit)
          Protocol.all
   in
+  (* Test with lazy parameters *)
+  let () =
+    (* Temporary files cannot be created outside of tests. If you want
+       to paramtrize your test by such a value, it can be created in a
+       lazy parameter *)
+    Parametric.(
+      parameterize
+        (Lazy.list
+           ~name:"temp-file"
+           [
+             ("temp_file1", fun () -> Temp.file "foo");
+             ("temp_file2", fun () -> Temp.file "bar");
+           ]))
+    @@ Test.register_parametric
+         ~__FILE__
+         ~title:"Example non-parametric test"
+         ~tags:["my"; "tags"]
+         (* The test does not really need [_temp_value_name], it's another wart *)
+         (fun (_temp_value_name, temp_file) ->
+           (* It is unfortunate by the thunk has to force inside the test function.
+              It could be circumvented by creating [Test.register_lazy_parametric]
+              but would create another set of register functions :( *)
+           let temp_file = temp_file () in
+           Log.info "My parametric test using the lazy temp-file %S" temp_file ;
+           unit)
+  in
+  (* Test with arbitrary parameters *)
+  let () =
+    Parametric.(parameterize Arbitrary.int)
+    @@ Test.register_parametric
+         ~__FILE__
+         ~title:"Example test with arbitrary values"
+         ~tags:["my"; "tags"]
+         (* Again the [_idx] tag here is a wart that could be solved in the same manner as above ... *)
+         (fun (_idx, arb_int_value) ->
+           Log.info "My arbitrary test, value %d" arb_int_value ;
+           unit)
+  in
+  let () =
+    Parametric.(parameterize (Arbitrary.of_qcheck QCheck.int))
+    @@ Test.register_parametric
+         ~__FILE__
+         ~title:"Example test with arbitrary values from a QCheck generator"
+         ~tags:["my"; "tags"]
+         (* Again the [_idx] tag here is a wart that could be solved in the same manner as above ... *)
+         (fun (_idx, arb_int_value) ->
+           Log.info "My arbitrary test, value %d" arb_int_value ;
+           unit)
+  in
+  let () =
+    Parametric.(parameterize Arbitrary.(pair int float))
+    @@ Test.register_parametric
+         ~__FILE__
+         ~title:"Example test with pairs of arbitrary values"
+         ~tags:["my"; "tags"]
+         (* Again the [_idx] tag here is a wart that could be solved in the same manner as above ... *)
+         (fun (_idx, (arb_int_value, arb_float_value)) ->
+           Log.info
+             "My arbitrary test, value %d and %f"
+             arb_int_value
+             arb_float_value ;
+           unit)
+  in
+
   ()
