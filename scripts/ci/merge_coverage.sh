65d52e82b877b48c48668c1caaa29bf69e56db4c
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-03-14 09:29:27 +0100

    Store coverage traces pre-merge

--- scripts/ci/merge_coverage.sh
+++ scripts/ci/merge_coverage.sh
@@ -11,7 +11,7 @@ if echo "${CI_MERGE_REQUEST_LABELS:-}" | grep -q '\(^\|,\)ci--no-coverage\($\|,\
 elif bisect-ppx-report merge --coverage-path "$BISECT_FILE" "$COVERAGE_MERGED".coverage; then
     # Merge was successful, meaning that no corrupted files were found
     COVERAGE_MERGED="$COVERAGE_MERGED".coverage
-    rm "$BISECT_FILE"*.coverage || true
+    # rm "$BISECT_FILE"*.coverage || true
     mv "$COVERAGE_MERGED" "$BISECT_FILE"
     echo "Merged coverage files to ${BISECT_FILE}/${COVERAGE_MERGED}"
 else
