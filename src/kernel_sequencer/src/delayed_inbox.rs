1cd3260c9c96cf4bacc5e6c77144a428636334ed
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-05-16 13:48:13 +0200

    kernel/sequencer: handle the different messages

--- src/kernel_sequencer/src/delayed_inbox.rs
+++ src/kernel_sequencer/src/delayed_inbox.rs
@@ -3,13 +3,17 @@
 //
 // SPDX-License-Identifier: MIT
 
+use tezos_data_encoding::nom::NomReader;
 use tezos_smart_rollup_host::{
     input::Message,
-    metadata::RollupMetadata,
+    metadata::{RollupMetadata, RAW_ROLLUP_ADDRESS_SIZE},
     runtime::{Runtime, RuntimeError},
 };
 
-use crate::routing::FilterBehavior;
+use crate::{
+    message::{Framed, KernelMessage, Sequence, SetSequencer},
+    routing::FilterBehavior,
+};
 
 /// Return a message from the inbox
 ///
@@ -30,10 +34,61 @@ pub fn read_input<Host: Runtime>(
             None => return Ok(None), // No more messages to be processed
             Some(msg) => {
                 let payload = msg.as_ref();
-                if filter_behavior.predicate(payload, &raw_rollup_address) {
-                    return Ok(Some(msg)); // The message is needed by the kernel so it's returned
+                let message = KernelMessage::nom_read(payload);
+                match message {
+                    Ok((_, KernelMessage::Message(msg))) => {
+                        handle_message(msg, filter_behavior, &raw_rollup_address)
+                    }
+                    Ok((_, KernelMessage::Sequence(framed))) => {
+                        handle_sequence_message(framed, &raw_rollup_address)
+                    }
+                    Ok((_, KernelMessage::SetSequencer(framed))) => {
+                        handle_set_sequencer_message(framed, &raw_rollup_address)
+                    }
+                    Err(_) => {}
                 }
             }
         }
+    }
+}
+
+/// Handle Sequence message
+fn handle_sequence_message(
+    framed: Framed<Sequence>,
+    rollup_address: &[u8; RAW_ROLLUP_ADDRESS_SIZE],
+) {
+    let Framed {
+        destination,
+        payload: _,
+    } = framed;
+
+    if destination.hash().as_ref() == rollup_address {
+        // process the sequence
+    }
+}
+
+fn handle_set_sequencer_message(
+    framed: Framed<SetSequencer>,
+    rollup_address: &[u8; RAW_ROLLUP_ADDRESS_SIZE],
+) {
+    let Framed {
+        destination,
+        payload: _,
+    } = framed;
+
+    if destination.hash().as_ref() == rollup_address {
+        // process the set sequencer message
+    }
+}
+
+/// Handle messages
+fn handle_message(
+    user_message: Vec<u8>,
+    filter_behavior: FilterBehavior,
+    rollup_address: &[u8; RAW_ROLLUP_ADDRESS_SIZE],
+) {
+    // Check if the message should be included in the delayed inbox
+    if filter_behavior.predicate(user_message.as_ref(), rollup_address) {
+        // add the message to the delayed inbox
     }
 }
