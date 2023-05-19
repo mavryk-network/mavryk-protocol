94fa2ca7dde850feb64c8137894d4bb59ef1f127
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-05-19 11:55:19 +0200

    kernel/sequencer: add set sequencer msg definition

--- src/kernel_sequencer/src/message.rs
+++ src/kernel_sequencer/src/message.rs
@@ -2,13 +2,18 @@
 //
 // SPDX-License-Identifier: MIT
 
-use nom::{bytes::complete::tag, combinator::map, sequence::preceded};
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    combinator::{all_consuming, map},
+    sequence::preceded,
+};
 use tezos_crypto_rs::hash::Signature;
 use tezos_data_encoding::{
     enc::{self, BinResult, BinWriter},
     nom::{NomReader, NomResult},
 };
-use tezos_smart_rollup_encoding::smart_rollup::SmartRollupAddress;
+use tezos_smart_rollup_encoding::{public_key::PublicKey, smart_rollup::SmartRollupAddress};
 
 /// Trait that indicates what is the tag of the message in the Framing protocol
 pub trait Tag {
@@ -55,9 +60,27 @@ impl Tag for Sequence {
     }
 }
 
+/// Message to set the appropriate sequencer
+///
+/// This message should be sent by the admin public key
+/// This admin key should sign the new sequencer public key
+#[derive(NomReader, BinWriter, Clone, Debug, PartialEq, Eq)]
+pub struct SetSequencer {
+    admin_public_key: PublicKey,
+    sequencer_public_key: PublicKey,
+    signature: Signature,
+}
+
+impl Tag for SetSequencer {
+    fn tag() -> u8 {
+        1 // Tag of the SetSequencer
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum KernelMessage {
     Sequence(Framed<Sequence>),
+    SetSequencer(Framed<SetSequencer>),
 }
 
 impl<P> NomReader for Framed<P>
@@ -105,10 +128,16 @@ where
 
 impl NomReader for KernelMessage {
     fn nom_read(input: &[u8]) -> NomResult<Self> {
-        map(
-            preceded(tag([1]), Framed::<Sequence>::nom_read),
-            KernelMessage::Sequence,
-        )(input)
+        all_consuming(alt((
+            all_consuming(map(
+                preceded(tag([1]), Framed::<Sequence>::nom_read),
+                KernelMessage::Sequence,
+            )),
+            all_consuming(map(
+                preceded(tag([1]), Framed::<SetSequencer>::nom_read),
+                KernelMessage::SetSequencer,
+            )),
+        )))(input)
     }
 }
 
@@ -120,6 +149,11 @@ impl BinWriter for KernelMessage {
                 enc::put_byte(&0x01, output);
                 sequencer.bin_write(output)?;
             }
+            KernelMessage::SetSequencer(set_sequencer) => {
+                // external message tag
+                enc::put_byte(&0x01, output);
+                set_sequencer.bin_write(output)?;
+            }
         }
         Ok(())
     }
@@ -130,6 +164,7 @@ mod tests {
     use crate::message::Framed;
 
     use super::{KernelMessage, Sequence};
+    use crate::message::SetSequencer;
     use tezos_crypto_rs::hash::{SecretKeyEd25519, SeedEd25519};
     use tezos_data_encoding::enc::BinWriter;
     use tezos_data_encoding::nom::NomReader;
@@ -165,6 +200,34 @@ mod tests {
         // Serializing
         let mut bin: Vec<u8> = Vec::new();
         sequence.bin_write(&mut bin).unwrap();
+
+        // Deserializing
+        let (_, msg_read) = KernelMessage::nom_read(&bin).expect("deserialization should work");
+
+        assert_eq!(msg_read, sequence);
+    }
+
+    #[test]
+    fn test_set_sequencer_serialization() {
+        let (public_key, secret) =
+            key_pair("edsk3a5SDDdMWw3Q5hPiJwDXUosmZMTuKQkriPqY6UqtSfdLifpZbB");
+        let signature = secret.sign([0x0]).expect("sign should work");
+
+        let sequence = KernelMessage::SetSequencer(Framed {
+            destination: SmartRollupAddress::from_b58check("sr1EzLeJYWrvch2Mhvrk1nUVYrnjGQ8A4qdb")
+                .expect("decoding should work"),
+            payload: SetSequencer {
+                admin_public_key: public_key.clone(),
+                sequencer_public_key: public_key,
+                signature,
+            },
+        });
+
+        // Serializing
+        let mut bin: Vec<u8> = Vec::new();
+        sequence.bin_write(&mut bin).unwrap();
+
+        println!("{:?}", bin);
 
         // Deserializing
         let (_, msg_read) = KernelMessage::nom_read(&bin).expect("deserialization should work");
