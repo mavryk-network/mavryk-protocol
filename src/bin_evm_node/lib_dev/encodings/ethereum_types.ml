b5ae76e35865520f93ca623711b29b6a2824442c
Antonio Locascio <antonio.locascio@nomadic-labs.com>
2023-10-26 14:57:56 +0200

    EVM/Proxy: types and encodings for getLogs RPC

--- src/bin_evm_node/lib_dev/encodings/ethereum_types.ml
+++ src/bin_evm_node/lib_dev/encodings/ethereum_types.ml
@@ -164,7 +164,7 @@ let decode_hash bytes = Hash (decode_hex bytes)
 type transaction_log = {
   address : address;
   topics : hash list;
-  data : hash;
+  data : hex;
   blockNumber : quantity;
   transactionHash : hash;
   transactionIndex : quantity;
@@ -181,7 +181,7 @@ let transaction_log_body_from_rlp = function
             | Rlp.Value bytes -> decode_hash bytes
             | _ -> raise (Invalid_argument "Expected hash representing topic"))
           topics,
-        decode_hash data )
+        decode_hex data )
   | _ ->
       raise
         (Invalid_argument "Expected list of 3 elements representing log body")
@@ -232,7 +232,7 @@ let transaction_log_encoding =
     (obj9
        (req "address" address_encoding)
        (req "topics" (list hash_encoding))
-       (req "data" hash_encoding)
+       (req "data" hex_encoding)
        (req "blockNumber" quantity_encoding)
        (req "transactionHash" hash_encoding)
        (req "transactionIndex" quantity_encoding)
@@ -991,3 +991,76 @@ let transaction_nonce raw_tx =
     match bytes |> String.to_bytes |> Rlp.decode with
     | Ok (Rlp.List [Value nonce; _; _; _; _; _; _; _; _]) -> decode_number nonce
     | _ -> raise (Invalid_argument "Expected a List of 9 elements")
+(* Event filter, see
+   https://ethereum.org/en/developers/docs/apis/json-rpc/#eth_getlogs *)
+
+type filter_topic = One of hash | Or of hash list
+
+let filter_topic_encoding =
+  let open Data_encoding in
+  union
+    [
+      case
+        ~title:"one"
+        (Tag 0)
+        hash_encoding
+        (function One hash -> Some hash | _ -> None)
+        (fun hash -> One hash);
+      case
+        ~title:"or"
+        (Tag 1)
+        (list hash_encoding)
+        (function Or l -> Some l | _ -> None)
+        (fun l -> Or l);
+    ]
+
+type filter = {
+  from_block : block_param option;
+  to_block : block_param option;
+  address : address option;
+  topics : filter_topic option list option;
+  block_hash : block_hash option;
+}
+
+let filter_encoding =
+  let open Data_encoding in
+  conv
+    (fun {from_block; to_block; address; topics; block_hash} ->
+      (from_block, to_block, address, topics, block_hash))
+    (fun (from_block, to_block, address, topics, block_hash) ->
+      {from_block; to_block; address; topics; block_hash})
+    (obj5
+       (opt "fromBlock" block_param_encoding)
+       (opt "toBlock" block_param_encoding)
+       (opt "address" address_encoding)
+       (opt "topics" (list @@ option filter_topic_encoding))
+       (opt "blockHash" block_hash_encoding))
+
+type filter_changes =
+  | Block_filter of block_hash
+  | Pending_transaction_filter of hash
+  | Log of transaction_log
+
+let filter_changes_encoding =
+  let open Data_encoding in
+  union
+    [
+      case
+        ~title:"block"
+        (Tag 0)
+        block_hash_encoding
+        (function Block_filter hash -> Some hash | _ -> None)
+        (fun hash -> Block_filter hash);
+      case
+        ~title:"pending_transaction"
+        (Tag 1)
+        hash_encoding
+        (function Pending_transaction_filter hash -> Some hash | _ -> None)
+        (fun hash -> Pending_transaction_filter hash);
+      case
+        ~title:"log"
+        (Tag 2)
+        transaction_log_encoding
+        (function Log f -> Some f | _ -> None)
+        (fun f -> Log f);
+    ]
