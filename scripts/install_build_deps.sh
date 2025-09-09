05d6efcc5055329d3258d90db21580010f246929
root <tristan.allaire@alarch.fr>
2025-09-09 08:58:16 +0000

    Findlib package switched

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -65,9 +65,9 @@ fi
 
 eval "$(opam env --shell=sh)"
 
-opam install --yes --no-checksums tezos-rust-libs.1.6
+OPAMASSUMEDEPEXTS=true opam install --yes --no-checksums tezos-rust-libs.1.6
 opam pin add -n ocamlfind.1.9.6 http://download2.camlcity.org/download/findlib-1.9.6.tar.gz
-opam install --yes ocamlfind.1.9.6
+OPAMASSUMEDEPEXTS=true opam install --yes ocamlfind.1.9.6
 
 # Check if the default opam repo was set in this switch
 default_switch=
