f16b28420f89fd74f663c66af85b025bef25f678
Tristan Allaire <tristan.allaire@alarch.fr>
2026-05-07 13:19:17 +0900

    Riscv regressions fixed

--- src/risc_v/interpreter/src/machine_state/csregisters.rs
+++ src/risc_v/interpreter/src/machine_state/csregisters.rs
@@ -256,16 +256,10 @@ pub enum CSRegister {
     pmpaddr62 = 0x3EE,
     pmpaddr63 = 0x3EF,
 
-    // Machine Non-Maskable Interrupt Handling
-    // The draft `Smrnmi` extension is not supported in objdump, printing
-    // CSR address directly instead
-    #[strum(to_string = "0x740")]
+    // Machine Non-Maskable Interrupt Handling (Smrnmi extension)
     mnscratch = 0x740,
-    #[strum(to_string = "0x741")]
     mnepc = 0x741,
-    #[strum(to_string = "0x742")]
     mncause = 0x742,
-    #[strum(to_string = "0x744")]
     mnstatus = 0x744,
 
     // Machine Counter/Timers
